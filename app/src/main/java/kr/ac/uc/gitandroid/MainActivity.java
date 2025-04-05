2dd6c7f0b1c36a8219e5d27e8e9130d8b519cca3
jeayeol <162972943+invigilble@users.noreply.github.com>
2025-04-05 22:36:21 +0900

    메인액티비티 변경

--- app/src/main/java/kr/ac/uc/gitandroid/MainActivity.java
+++ app/src/main/java/kr/ac/uc/gitandroid/MainActivity.java
@@ -24,56 +24,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TextView 초기화
-
         // 숫자 버튼 초기화
-
         // 연산자 버튼 초기화
-
         // 숫자 버튼 클릭 리스너 설정
 
         // 숫자 버튼에 리스너 적용
-
         // 연산자 버튼 클릭 리스너 설정
 
         // 연산자 버튼에 리스너 적용
-
         // 등호 버튼 클릭 리스너
-        btnEqual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    secondNumber = Double.parseDouble(tvDisplay.getText().toString());
-                    double result = 0;
 
-                    switch (operation) {
-                        case "+":
-                            result = firstNumber + secondNumber;
-                            break;
-                        case "-":
-                            result = firstNumber - secondNumber;
-                            break;
-                        case "*":
-                            result = firstNumber * secondNumber;
-                            break;
-                    }
-
-                    // 정수인 경우 소수점 제거 (예: 5.0 -> 5)
-                    if (result == (long) result) {
-                        tvDisplay.setText(String.valueOf((long) result));
-                    } else {
-                        tvDisplay.setText(String.valueOf(result));
-                    }
-
-                    isNewInput = true;
-                } catch (NumberFormatException e) {
-                    tvDisplay.setText("Error");
-                    isNewInput = true;
-                }
-            }
-        });
 
         // C 버튼 (현재 입력 취소)
 
         // AC 버튼 (모든 계산 취소)
-    }
+
 }
