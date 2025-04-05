466e958149ffc529f1ae29e98421014f6dd3c447
jeayeol <162972943+invigilble@users.noreply.github.com>
2025-04-05 21:50:56 +0900

    메인액티비티 변경

--- app/src/main/java/kr/ac/uc/gitandroid/MainActivity.java
+++ app/src/main/java/kr/ac/uc/gitandroid/MainActivity.java
@@ -24,139 +24,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TextView 초기화
-        tvDisplay = findViewById(R.id.tvDisplay);
-        tvDisplay.setText("0");
+
 
         // 숫자 버튼 초기화
-        btn0 = findViewById(R.id.btn0);
-        btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-        btn3 = findViewById(R.id.btn3);
-        btn4 = findViewById(R.id.btn4);
-        btn5 = findViewById(R.id.btn5);
-        btn6 = findViewById(R.id.btn6);
-        btn7 = findViewById(R.id.btn7);
-        btn8 = findViewById(R.id.btn8);
-        btn9 = findViewById(R.id.btn9);
+
 
         // 연산자 버튼 초기화
-        btnPlus = findViewById(R.id.btnPlus);
-        btnMinus = findViewById(R.id.btnMinus);
-        btnMultiply = findViewById(R.id.btnMultiply);
-        btnEqual = findViewById(R.id.btnEqual);
-        btnCancel = findViewById(R.id.btnCancel);
-        btnAllClear = findViewById(R.id.btnAllClear);
+
 
         // 숫자 버튼 클릭 리스너 설정
-        View.OnClickListener numberClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button button = (Button) v;
-                String digit = button.getText().toString();
 
-                if (isNewInput) {
-                    tvDisplay.setText(digit);
-                    isNewInput = false;
-                } else {
-                    String currentText = tvDisplay.getText().toString();
-                    if (currentText.equals("0")) {
-                        tvDisplay.setText(digit);
-                    } else {
-                        tvDisplay.setText(currentText + digit);
-                    }
-                }
-            }
-        };
 
         // 숫자 버튼에 리스너 적용
-        btn0.setOnClickListener(numberClickListener);
-        btn1.setOnClickListener(numberClickListener);
-        btn2.setOnClickListener(numberClickListener);
-        btn3.setOnClickListener(numberClickListener);
-        btn4.setOnClickListener(numberClickListener);
-        btn5.setOnClickListener(numberClickListener);
-        btn6.setOnClickListener(numberClickListener);
-        btn7.setOnClickListener(numberClickListener);
-        btn8.setOnClickListener(numberClickListener);
-        btn9.setOnClickListener(numberClickListener);
+
 
         // 연산자 버튼 클릭 리스너 설정
-        View.OnClickListener operatorClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button button = (Button) v;
-                String op = button.getText().toString();
 
-                try {
-                    firstNumber = Double.parseDouble(tvDisplay.getText().toString());
-                    operation = op;
-                    isNewInput = true;
-                } catch (NumberFormatException e) {
-                    tvDisplay.setText("Error");
-                    isNewInput = true;
-                }
-            }
-        };
 
         // 연산자 버튼에 리스너 적용
-        btnPlus.setOnClickListener(operatorClickListener);
-        btnMinus.setOnClickListener(operatorClickListener);
-        btnMultiply.setOnClickListener(operatorClickListener);
+
 
         // 등호 버튼 클릭 리스너
-        btnEqual.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    secondNumber = Double.parseDouble(tvDisplay.getText().toString());
-                    double result = 0;
 
-                    switch (operation) {
-                        case "+":
-                            result = firstNumber + secondNumber;
-                            break;
-                        case "-":
-                            result = firstNumber - secondNumber;
-                            break;
-                        case "*":
-                            result = firstNumber * secondNumber;
-                            break;
-                    }
-
-                    // 정수인 경우 소수점 제거 (예: 5.0 -> 5)
-                    if (result == (long) result) {
-                        tvDisplay.setText(String.valueOf((long) result));
-                    } else {
-                        tvDisplay.setText(String.valueOf(result));
-                    }
-
-                    isNewInput = true;
-                } catch (NumberFormatException e) {
-                    tvDisplay.setText("Error");
-                    isNewInput = true;
-                }
-            }
-        });
 
         // C 버튼 (현재 입력 취소)
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvDisplay.setText("0");
-                isNewInput = true;
-            }
-        });
+
 
         // AC 버튼 (모든 계산 취소)
-        btnAllClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvDisplay.setText("0");
-                firstNumber = 0;
-                secondNumber = 0;
-                operation = "";
-                isNewInput = true;
-            }
-        });
+
     }
 }
